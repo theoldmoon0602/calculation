26ea5fd6109a3e013309d1978f82ee75d029a0c4
theoldmoon0602 <theoldmoon0602@users.noreply.github.com>
2018-04-13 16:04:37 +0900

    Add files via upload

---
+++ differential.d
@@ -0,0 +1,17 @@
+﻿import std.stdio;
+
+auto differential(F)(F f)
+{
+    auto h = float.epsilon;
+    return (double x) {
+        return (f(x + h) - f(x - h)) / (h * 2);
+    };
+}
+
+void main()
+{
+    auto f = (double x) => x*x;
+    auto df = differential(f);
+    
+    writeln(df(10));
+}
