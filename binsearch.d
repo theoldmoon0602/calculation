55ae961fc1b095102eba5666b4caa7028e32418a
theoldmoon0602 <theoldmoon0602@users.noreply.github.com>
2018-04-13 15:48:51 +0900

    Create binsearch.d

---
+++ binsearch.d
@@ -0,0 +1,27 @@
+import std.stdio;
+
+auto binsearch(F)(F f, double s, double e, double eps)
+{
+    while (true) {
+        auto m = s + (e - s) / 2;
+        if (e - s <= eps) {
+            return m;
+        }
+        auto r = f(m);
+        if (r == 0) {
+            return m;
+        }
+        else if (r < 0) {
+            s = m;
+        }
+        else {
+            e = m;
+        }
+    }
+}
+
+void main()
+{
+    auto f = (double x) => x*x - 3;
+    writefln("%.10f", binsearch(f, double.min_normal, double.max, double.epsilon));
+}
