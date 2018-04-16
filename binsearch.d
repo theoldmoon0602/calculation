e2f3f9c9fec530b8e625b69e263e6d1946faf295
theoldmoon0602 <theoldmoon0602@users.noreply.github.com>
2018-04-16 11:23:48 +0900

    Update binsearch.d

--- binsearch.d
+++ binsearch.d
@@ -1,29 +1,5 @@
 import std.stdio;
-
-auto binsearch(F)(F f, double s, double e, double eps)
-{
-    if (s > e) {
-        import std.algorithm : swap;
-        swap(s, e);
-    }
-    
-    while (true) {
-        auto m = s + (e - s) / 2;
-        if (e - s <= eps) {
-            return m;
-        }
-        auto r = f(m);
-        if (r == 0) {
-            return m;
-        }
-        else if (r < 0) {
-            s = m;
-        }
-        else {
-            e = m;
-        }
-    }
-}
+import lib.binsearch;
 
 void main()
 {
