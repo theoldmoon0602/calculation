1bea019a567778b718966eb82b037968b5eab43b
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-04-21 09:36:36 +0900

    updated

--- differential.d
+++ differential.d
@@ -1,10 +1,13 @@
 import std.stdio;
-import lib.differential;
+import lib.differential : differential;
 
 void main()
 {
     auto f = (double x) => x*x;
     auto df = differential(f);
-    
     writeln(df(10));
+
+    auto g = (double x, double y) => x*x + y*y;
+    auto dg = differential((double x) => g(x, 3));
+    writeln(dg(3));
 }
