7a934e42d2b79486035df616c308de35635dee5a
theoldmoon0602 <theoldmoon0602@users.noreply.github.com>
2018-06-29 15:33:21 +0900

    Update simpson.d

--- lib/simpson.d
+++ lib/simpson.d
@@ -12,7 +12,7 @@ do
     auto h = (to - from) / n;
     double s = f(from) + f(to);
     
-    for (int i = 1; i < n - 1; i++) {
+    for (int i = 1; i < n ; i++) {
         auto x = from + i * h;
         s += ((i % 2 == 0) ? 2 : 4) * f(x);
     }
